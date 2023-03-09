0439b716eed91975203bc54f043a79f698318b00
proussel <pierre.roussel01@free.fr>
2023-03-09 17:28:58 +0100

    Add strapi

---
+++ apps/strapi/config/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', 'db4free.net'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'karinelefort'),
+      user: env('DATABASE_USERNAME', 'karinelefort'),
+      password: env('DATABASE_PASSWORD', '645ZEOtio'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
+// karinelefort
+// karinelefort
+// 645ZEOtio
