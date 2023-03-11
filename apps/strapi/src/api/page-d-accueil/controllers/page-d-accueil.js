93c239cc976e31de9e6b0a6679f6bc75e29d9007
proussel <pierre.roussel01@free.fr>
2023-03-11 11:24:56 +0100

    Remove strapi from front

--- apps/strapi/src/api/page-d-accueil/controllers/page-d-accueil.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * page-d-accueil controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::page-d-accueil.page-d-accueil');
--- apps/strapi/src/api/page-d-accueil/routes/page-d-accueil.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * page-d-accueil router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::page-d-accueil.page-d-accueil');
