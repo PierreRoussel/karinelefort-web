c03271667ce031d83d7f3c3df96bd8a9c8766ffe
proussel <pierre.roussel01@free.fr>
2023-03-14 15:20:54 +0100

    responsive all

--- src/components/CtaBtn/CtaBtn.tsx
+++ src/components/CtaBtn/CtaBtn.tsx
@@ -2,8 +2,15 @@ import { useNavigate } from '@solidjs/router';
 import './ctaBtn.scss';
 function CtaBtn(props: any) {
   const navigate = useNavigate();
+  const link =
+    props.link === 'https://www.fotostudio.io/lead_forms/3762'
+      ? '/contact'
+      : props.link;
   return (
-    <button class={`basic-cta-btn ${props.reversed?'reversed':''}`} onClick={() => navigate(props.link)}>
+    <button
+      class={`basic-cta-btn ${props.reversed ? 'reversed' : ''}`}
+      onClick={() => navigate(link)}
+    >
       <div class='basic-cta-btn__outline'></div>
       {props.btnTitle}
     </button>
--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -12,7 +12,6 @@ function Contact() {
           ></iframe>
         </div>
       </div>
-      <CtaBanner />
     </div>
   );
 }
