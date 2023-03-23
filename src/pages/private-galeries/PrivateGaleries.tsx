4fec76a16ed63036d4cc70618844e68ef1840812
proussel <pierre.roussel01@free.fr>
2023-03-23 17:12:06 +0100

    beforeSSR META

--- src/pages/private-galeries/PrivateGaleries.tsx
+++ src/pages/private-galeries/PrivateGaleries.tsx
@@ -1,63 +1,98 @@
 import CtaBanner from '../../components/cta-banner/CtaBanner';
-import { For, onMount } from 'solid-js';
-import './privateGaleries.scss';
+import {
+  For,
+  createEffect,
+  createResource,
+  Suspense,
+  createSignal,
+} from 'solid-js';
 import { observer } from '../../modules/utils';
-function PrivateGaleries() {
-  const galeries = [
-    {
-      title: 'Julie',
-      link: '#',
-    },
-    {
-      title: 'Arnaud',
-      link: '#',
-    },
-    {
-      title: 'Duchamp',
-      link: '#',
-    },
-    {
-      title: 'Luche',
-      link: '#',
-    },
-    {
-      title: 'Kempen',
-      link: '#',
-    },
-    {
-      title: 'Agathe',
-      link: '#',
-    },
-  ];
+import { fetchAPI } from '../../modules/api';
+import { APIParams } from '../../modules/api_types';
+import './privateGaleries.scss';
+import SpiralLoader from '../../components/loaders/SpiralLoader';
+import { Portal } from 'solid-js/web';
 
-  onMount(()=>{
-    const targets = document.querySelectorAll('.reveal');
-    targets.forEach(function (target) {
-      observer.observe(target);
-    });
-  })
+function PrivateGaleries() {
+  const [galeryIdx, setGaleryIdx] = createSignal(-1);
+  const urlParamsObject: APIParams = {
+    publicationState: 'live',
+    populate: '*',
+  };
+  const [galeries] = createResource(() =>
+    fetchAPI('private-galeries', urlParamsObject)
+  );
+  createEffect(() => {
+    if (!galeries.loading) {
+      const targets = document.querySelectorAll('.reveal');
+      targets.forEach(function (target) {
+        observer.observe(target);
+      });
+    }
+  });
+  function selectGalery(galeryItem: any) {
+    //@ts-ignore
+    setGaleryIdx(galeries()?.data.indexOf(galeryItem));
+    console.log('galeryItem', galeryItem);
+  }
   return (
     <div class='private-galeries'>
       <div class='galeries__header reveal'>
         <h1>Vos photos</h1>
         <span>Votre galerie privée suite à une séance avec moi</span>
       </div>
-      <ul class='reveal'>
-        <For each={galeries}>
-          {(galery: any, index) => {
-            return (
-              <li>
-                <div
-                  class='private-galery-item fade-in-bck'
-                  style={`animation-delay: ${index() * 120}ms`}
-                >
-                  <span class='lock'></span> {galery.title}
-                </div>
-              </li>
-            );
-          }}
-        </For>
-      </ul>
+      <Suspense fallback={<SpiralLoader />}>
+        <ul class='reveal'>
+          <For each={galeries()?.data}>
+            {(galery: any, index) => {
+              return (
+                <li onClick={() => selectGalery(galery)}>
+                  <div
+                    class='private-galery-item fade-in-bck'
+                    style={`animation-delay: ${index() * 120}ms`}
+                  >
+                    <span class='lock'></span> {galery.attributes.Nom}
+                  </div>
+                </li>
+              );
+            }}
+          </For>
+        </ul>
+        <Portal>
+          <div
+            class={`password-modal-overlay ${
+              !galeries.loading && galeryIdx() !== -1 ? 'show-modal' : ''
+            }`}
+            onClick={() => setGaleryIdx(-1)}
+          ></div>
+          <div
+            class={`password-modal-content reveal-loaded ${
+              !galeries.loading && galeryIdx() !== -1 ? 'show-modal' : ''
+            }`}
+          >
+            <span>
+              Accéder à la galerie{' '}
+              <i>
+                "
+                {galeryIdx() !== -1 &&
+                  galeries()?.data[galeryIdx()].attributes.Nom}
+              </i>
+              "
+            </span>
+            <div class='input-container'>
+              <input type='text' id='input' required />
+              <label for='input' class='label'>
+                Mot de passe
+              </label>
+              <div class='underline'></div>
+            </div>
+            <button class={`btn`}>
+                <div class='btn__outline'></div>
+                Entrer
+              </button>
+          </div>
+        </Portal>
+      </Suspense>
       <CtaBanner />
     </div>
   );
